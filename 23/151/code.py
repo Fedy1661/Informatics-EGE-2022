4833f8423e834cadca3e0afb64cf27a44f0f142d
Fedor <fedykonovalov@gmail.com>
2021-10-15 17:54:33 +0300

    Add 149-151 task

--- 23/151/code.py
+++ 23/151/code.py
@@ -1,7 +1,10 @@
-def numProg( start, x ):
+def numProg(start, x):
     if x < start: return 0
     if x == start: return 1
-    start3 = start+1 if start % 2 == 0 else start+2
-    return numProg(start+1,x) + numProg(start*2,x) + numProg(start3, x)
+    if start % 2 == 0:
+        start3 = start + 1
+    else:
+        start3 = start + 2
+    return numProg(start + 1, x) + numProg(start * 2, x) + numProg(start3, x)
 
-print( numProg(3,9)*numProg(9,17)*numProg(17,25) )
+print(numProg(3, 9) * numProg(9, 17) * numProg(17, 25))

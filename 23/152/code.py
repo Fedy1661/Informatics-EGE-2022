2c37579582aeb3858ad452dc821dc4f82e9759a3
Fedor <fedykonovalov@gmail.com>
2021-10-15 22:13:00 +0300

    Add 152 task

--- 23/152/code.py
+++ 23/152/code.py
@@ -3,7 +3,7 @@ def numProg(start, x):
     if x == start: return 1
     return numProg(start + 1, x) + numProg(start + 2, x)
 
-path = numProg(11, 17) * numProg(17, 23) * numProg(23, 29)
-path2 = numProg(11, 17) * numProg(17, 29)
-path3 = numProg(11, 23) * numProg(23, 29)
-print(path3 + path2 - path)
+part_1 = numProg(11, 17) * numProg(17, 29)
+part_2 = numProg(11, 23) * numProg(23, 29)
+part_general = numProg(11, 17) * numProg(17, 23) * numProg(23, 29)
+print(part_1 + part_2 - part_general)
